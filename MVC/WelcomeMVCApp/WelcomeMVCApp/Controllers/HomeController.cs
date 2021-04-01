cfc3e3b3534d1d6b12a4d31e450440239da827c1
Shashank <shetyeshashank04@gmail.com>
2021-04-01 15:13:56 +0530

    Validated And Authentication of Form Done

--- MVC/WelcomeMVCApp/WelcomeMVCApp/Controllers/HomeController.cs
+++ MVC/WelcomeMVCApp/WelcomeMVCApp/Controllers/HomeController.cs
@@ -5,27 +5,27 @@ using System.Web;
 using System.Web.Mvc;
 using WelcomeMVCApp.Models;
 
+
 namespace WelcomeMVCApp.Controllers
 {
     public class HomeController : Controller
     {
         bool flag = false;
+      public static List<string> logincredentials = new List<string>();
+        
         
        public HomeController()
         {
             
         }
         
-        public void Index(string value)
-        {   for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("{0}",value);
-            }
+        //Display name
+        
+       public ActionResult Index()
+        {
+            return View();
         }
 
-
-
-       
         public ActionResult hello(string name)
         {
             ViewBag.MyString = name;
@@ -50,6 +50,9 @@ namespace WelcomeMVCApp.Controllers
             return View(user);
         }
 
+
+
+        //Display Date
         public ActionResult displayDate()
         {
             ViewBag.flag = flag;
@@ -64,5 +67,87 @@ namespace WelcomeMVCApp.Controllers
             ViewBag.name = frm["name"].ToString();
             return View();
         }
+
+
+        //ForM Validation
+        public ActionResult validate()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public ActionResult submit(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View("validate");
+        }
+
+
+        //Create Account
+        public ActionResult loginToAuthenticate()
+        {
+            return View();
+        }
+
+        public ActionResult authenticate(Login login)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                //  if (login.name.ToLower().Equals(name.ToLower()) && login.email.Equals("sndc@gmail.com") && login.password.Equals("shashank1"))
+                // {
+                //     return View("submit");
+                // }
+                string str = login.name+login.password;
+                logincredentials.Add(str);
+                Content("Account Created Successfully"); 
+
+            }
+            return View("accountcreated");
+        }
+
+        //Login after Creating An Account
+        public ActionResult login()
+        {  
+            return View();
+        }
+        public ActionResult loginresult(Login log)
+        {
+            logincredentials.Add("Shashankshashank1");
+            if (ModelState.IsValid)
+            {
+                int count = 0;
+                string str = log.name+log.password;
+                foreach(string i in logincredentials)
+                {
+                    if (i.Equals(str))
+                    {
+                        count++;
+                    }
+                    if (count == 1)
+                    {
+                        break;
+                    }
+                }
+                if (count == 1) { return View("submit"); }
+                
+
+            }
+            return View("loginfailed");
+        }
+
+
+        //Alert If Incorrect informatio
+        public ActionResult loginfailed()
+        {
+            return View();
+        }
+
+        
+
+
     }
 }
---
+++ MVC/WelcomeMVCApp/WelcomeMVCApp/Models/Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace WelcomeMVCApp.Models
+{
+    public class Login
+    {   [Required(ErrorMessage ="Name Required")]
+        public string name { get;set; }
+
+
+        [Required(ErrorMessage ="Not a Valid Email Address")]
+        [EmailAddress]
+        public string email { get; set; }
+
+
+        [Required(ErrorMessage ="Password Required")]
+        [StringLength(12,MinimumLength =8,ErrorMessage ="Password:Min=8 and Max=12")]
+        
+        public string password { get; set; }
+
+
+    }
+}
