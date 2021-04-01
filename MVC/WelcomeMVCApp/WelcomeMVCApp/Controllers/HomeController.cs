e85d3807abe5ba26dd12c334d89748cc9e937b66
Shashank <shetyeshashank04@gmail.com>
2021-04-01 11:38:46 +0530

    Lecture Work Uploaded

--- MVC/WelcomeMVCApp/WelcomeMVCApp/Controllers/HomeController.cs
+++ MVC/WelcomeMVCApp/WelcomeMVCApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WelcomeMVCApp.Models;
 
 namespace WelcomeMVCApp.Controllers
 {
@@ -42,9 +43,11 @@ namespace WelcomeMVCApp.Controllers
         {
             flag = true;
             ViewBag.flag = flag;
-            ViewBag.id = int.Parse(frm["id"]);
-            ViewBag.name =frm["name"].ToString() ;
-            return View();
+              ViewBag.tech = frm["Technology"].ToString();
+            //  ViewBag.id = int.Parse(frm["id"]);
+            var user = new User() { id = int.Parse(frm["id"]), Name = frm["name"].ToString() };
+          //  ViewBag.name =frm["name"].ToString() ;
+            return View(user);
         }
 
         public ActionResult displayDate()
